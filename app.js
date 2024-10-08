9a581a2bd0b296ec8ad9f19ff23327c9d33edf5c
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-10-08 03:39:16 -0300

    more

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
+//Midleware para servir contenido estáticos
+app.use(express.static("public"));
+
+/*
 app.get("/", function (req, res) {
   res.send("Hola Mundo");
 });
+*/
 
-app.get("/contacto", function (req, res) {
-  res.send("Contacto");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/public/index.html");
 });
 
-app.listen(3000);
+//Ruta para la pagina contacto
+app.get("/contacto", function (req, res) {
+  res.sendFile(__dirname + "/public/contacto.html");
+});
+
+app.get("*", function (req, res) {
+  res.sendFile(__dirname + "/public/404.html");
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
+});
